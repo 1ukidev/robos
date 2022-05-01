232b76112f8621de6c745ef12ebfb2cc80955f85
Leonardo / LuKi <leo.monteiro06@live.com>
2022-05-01 17:51:12 -0300

    Atualizando robôs

--- lukidev/RoboDois.java
+++ lukidev/RoboDois.java
@@ -1,64 +1,84 @@
-package tnq;
-import robocode.*;
-import java.awt.*;
+package lukidev;
 
-public class RoboDois extends AdvancedRobot
-{
-	int direcao = 1;
+import robocode.*;
+import java.awt.Color;
+
+public class RoboDois extends AdvancedRobot {
+	int sentido = 1;
+	boolean avancando;
 
 	public void run() {
 		setBodyColor(Color.blue);
-		setGunColor(Color.green);
-		setRadarColor(Color.green);
+		setGunColor(Color.red);
+		setRadarColor(Color.blue);
 		setBulletColor(Color.blue);
+		setRadarColor(Color.blue);
 
-		while(true) {
-			turnRight(5 * direcao);
+		while (true) {
+			turnRight(5 * sentido);
 			setAhead(40000);
+			avancando = true;
 			setTurnRight(90);
 			waitFor(new TurnCompleteCondition(this));
 			setTurnLeft(180);
 			waitFor(new TurnCompleteCondition(this));
 			setTurnRight(180);
-			waitFor(new TurnCompleteCondition(this));			
+			waitFor(new TurnCompleteCondition(this));
 		}
 	}
-	
+
+	// Ao escanear um robô
 	public void onScannedRobot(ScannedRobotEvent e) {
-		if(e.getBearing() >= 0) {
-			direcao = 1;
+		if (e.getBearing() >= 0) {
+			sentido = 1;
 		} else {
-			direcao = -1;
+			sentido = -1;
 		}
-		
+
 		turnRight(e.getBearing());
-		ahead(e.getDistance());
+		ahead(e.getDistance() - 100);
+		fire(2);
 		scan();
 	}
-	
-	public void onHitRobot(HitRobotEvent e) {
-		if(e.getBearing() >= 0) {
-			direcao = 1;
+
+	// Função para fazer o robô ir pra direção oposta
+	public void direcaoReversa() {
+		if (avancando) {
+			setBack(40000);
+			avancando = false;
 		} else {
-			direcao = -1;
+			setAhead(40000);
+			avancando = true;
 		}
+	}
+
+	// Ao atingir um robô
+	public void onHitRobot(HitRobotEvent e) {
+		if (e.getBearing() >= 0) {
+			sentido = 1;
+		} else {
+			sentido = -1;
+		}
+
 		turnRight(e.getBearing());
 		fire(3);
-
-		ahead(40);
+		scan();
 	}
 
+	// Ao ser atingido por uma bala
 	public void onHitByBullet(HitByBulletEvent e) {
-		setAhead(40000);
-		waitFor(new TurnCompleteCondition(this));
-		setTurnRight(90);
-		waitFor(new TurnCompleteCondition(this));
+		if (e.getBearing() >= 0) {
+			sentido = 1;
+		} else {
+			sentido = -1;
+		}
+
+		turnRight(e.getBearing());
+		fire(2);
 	}
 
+	// Ao bater na parede
 	public void onHitWall(HitWallEvent e) {
-		setAhead(40000);
-		waitFor(new TurnCompleteCondition(this));
-		setTurnRight(90);
-		waitFor(new TurnCompleteCondition(this));
-	}	
+		direcaoReversa();
+	}
 }
